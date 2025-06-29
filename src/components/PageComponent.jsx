9f1adaa31bdbf38bb3803c163b91a8cd06e75d1c
Mikhail <mikhail.polyakov.2006@bk.ru>
2025-06-29 22:45:26 +0300

    Half-brain solution, but it works ^-^

--- src/components/PageComponent.jsx
+++ src/components/PageComponent.jsx
@@ -1,18 +1,16 @@
-import {  useState } from "react";
+import {  useState, useRef } from "react";
 import Checkbox from '@mui/material/Checkbox';
 
 export default function PageComponent({neededObj, onProjectDelete}) {
     const [ enteredValue, setEnteredValue ] = useState("");
-    const [changed, setChanged] = useState(false);
-    const [taskDeleted, setTaskDeleted] = useState(false);
-    const [ chBoxStates, setChBoxStates ] = useState([]);
-    const {titleEntered, dateEntered, descriptionEntered, tasks} = neededObj;
+    const [ complitedCount, setComplited ] = useState(0);
+    const chkRefs = useRef([]); 
 
+    const {titleEntered, dateEntered, descriptionEntered, tasks} = neededObj;
     function handleAddANewTask(taskName) {
-        if(tasks.indexOf(taskName) === -1 && changed && taskName) {
+        if(tasks.indexOf(taskName) === -1 && taskName) {
             tasks.push(taskName);
-            setChBoxStates((prev) => [...prev, false]);
-            setChanged(false);
+            chkRefs.current = [ ...chkRefs.current, false]
             setEnteredValue("");
         }
         else {
@@ -20,33 +18,25 @@ export default function PageComponent({neededObj, onProjectDelete}) {
         }
     }
 
-    function handleChangeStateCheckbox(event, index) {
-        setChBoxStates((prev) => {
-            prev[index] = event.target.checked
-            console.log([...prev]);
-            return [...prev];
-        });
-    }
-
     const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
+    function handleChangeCheckboxValue(event, index) {
+        setComplited(prev => event.target.checked ? ++prev : --prev);
+        chkRefs.current[index] = event.target.checked;
+    }
+
     function handleChangeInputText(event) {
-        setChanged(true);
         setEnteredValue(event.target.value);
     }
 
     function handleDeleteTask(index) {
         tasks.splice(index, 1);
-        setChBoxStates(prev => {
-             prev.splice(index, 1);
-             console.log(prev);
-             return [...prev];
-        });
-        setTaskDeleted((prevState) => !prevState);
+        const currentCheck = chkRefs.current[index];
+        setComplited(prev => currentCheck ? --prev : setComplited(prev));
+        chkRefs.current.splice(index, 1);
     }
 
     return (
-        <>
         <section className="min-w-96 my-16 flex-grow flex flex-wrap h-fit">
             <div className="w-10/12">
                 <h1 className="font-bold text-4xl mb-4">{titleEntered}</h1>
@@ -64,23 +54,40 @@ export default function PageComponent({neededObj, onProjectDelete}) {
                     <input value={enteredValue} onChange={(event) => handleChangeInputText(event)} type="text" className="bg-gray-200 h-8 outline-none p-2 focus:border-b-2 border-gray-600"/>
                     <button onClick={() => handleAddANewTask(enteredValue)} className="mx-2 bg-transparent py-2 px-6 rounded-lg hover:bg-gray-100">Add Task</button>
                 </div>
-                <div>
-                    <ul className="rounded-md">
+                <div className="min-h-64">
+                    <ul className="rounded-md min-h-full">
                         {tasks.length > 0 ? tasks.map(item => 
-                                <li className="flex items-center" key={item}>
+                                <li className={chkRefs.current[tasks.indexOf(item)] ? "items-center hidden" : "flex items-center"} key={item}>
                                     <div className="mr-2">
-                                        <Checkbox {...label} onChange={(event) => handleChangeStateCheckbox(event, tasks.indexOf(item))} color="default"/>
+                                        <Checkbox {...label} onChange={(event) => handleChangeCheckboxValue(event, tasks.indexOf(item))} checked={false} color="default"/>
                                     </div>
                                     <div className="flex min-w-96 px-4 py-2 my-2 items-center bg-slate-100 transform duration-500 hover:bg-slate-200 flex-grow">
-                                        <p className={ chBoxStates[tasks.indexOf(item)] ? "flex-grow text-lg line-through" : "flex-grow text-lg"}>{item}</p>
+                                        <p className="flex-grow">{item}</p>
                                         <button onClick={() => handleDeleteTask(tasks.indexOf(item))} className="mr-2 bg-transparent py-2 px-6 rounded-lg transform duration-500 hover:text-red-500">Clear</button>
                                     </div>
                                 </li>
                         ) : <li className="text-stone-300 h-8 text-xl">* Add new task to see the result</li>}
                     </ul>
                 </div>
+                <hr className="bg-slate-700 h-1" />
+                <h2 className="text-xl font-semibold">Complited</h2>
+                {chkRefs.current.length === complitedCount && chkRefs.current.length != 0? <h2>Все задачи выполнены!</h2> : <h2>Выполненных задач: {complitedCount}</h2>}
+                <div>
+                    <ul className="rounded-md">
+                        {complitedCount > 0 ? tasks.map(item => 
+                                <li className={!chkRefs.current[tasks.indexOf(item)] ? "items-center hidden" : "flex items-center"} key={item}>
+                                    <div className="mr-2">
+                                        <Checkbox {...label} onChange={(event) => handleChangeCheckboxValue(event, tasks.indexOf(item))} checked={true} color="default"/>
+                                    </div>
+                                    <div className="flex min-w-96 px-4 py-2 my-2 items-center bg-slate-100 transform duration-500 hover:bg-slate-200 flex-grow">
+                                        <p className="flex-grow">{item}</p>
+                                        <button onClick={() => handleDeleteTask(tasks.indexOf(item))} className="mr-2 bg-transparent py-2 px-6 rounded-lg transform duration-500 hover:text-red-500">Clear</button>
+                                    </div>
+                                </li>
+                        ) : <li></li>}
+                    </ul>
+                </div>
             </div>
         </section>
-        </>
     );
 }
