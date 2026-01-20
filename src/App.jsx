9d5e7a0d2c61bc50296a7607cb1b2e554a5226f4
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2026-01-20 09:33:41 +0300

    SignUp ui is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './App.css';
+import SignUp from "./components/sign-up/SignUp.jsx"
 import TodoList from './TodoListComponent';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path="dashboard" element={<TodoList />} />
         <Route path="signIn" element={<div>SignIn Page</div>} />
-        <Route path="signUp" element={<div>SignUp Page</div>} />
+        <Route path="signUp" element={<SignUp />} />
         <Route path="forgotPassword" element={<div>Forgot Password Page</div>} />
         <Route path="/" element={<div>Welcome Page</div>} />
         <Route path="*" element={<div>Oops, this page doesn't exist</div>} />
---
+++ src/api/admin.js
@@ -0,0 +1,59 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SERVICE_ROLE_KEY, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
+  }
+})
+const adminAuthClient = supabase.auth.admin
+
+export async function fetchUsers() {
+    try {
+        const { data: { users }, error } = await adminAuthClient.listUsers();
+        return users
+    }
+    catch(e) {
+        throw e;
+    }
+}
+
+export async function fetchAdmins() {
+     try {
+        const { data, error } = await supabase
+        .from('admins')
+        .select()
+        return data;
+    }
+    catch(e) {
+        throw e;
+    }    
+}
+
+export async function createUser(uname, email, inpPassword, isAdmin) {
+  try {
+    if(isAdmin) {
+      const { data, error } = await adminAuthClient.createUser({
+        email: email,
+        password: inpPassword,
+        email_confirm: true,
+        user_metadata: { user_name: uname, isAdmin: true }
+      })
+      const { insertError } = await supabase
+        .from('admins')
+        .insert({ user_id: data.user.id })
+        }
+    else {
+      const { data, error } = await adminAuthClient.createUser({
+        email: email,
+        password: inpPassword,
+        email_confirm: true,
+        user_metadata: { user_name: uname}
+      })
+    }
+  }
+  catch(e) {
+    throw e;
+  }
+}
+
