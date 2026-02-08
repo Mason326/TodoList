730b1f78068776932f8a31031e4408a99f00ac72
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2026-02-08 13:57:18 +0300

    Updated imports and actualized init-db.sql script

--- src/api/supabase/supabase-utils/db.js
+++ src/api/supabase/supabase-utils/db.js
@@ -1,4 +1,4 @@
-import { supabase } from "../supabase/supabase-client";
+import { supabase } from "../supabase-client/index.js";
 
 export default async function fetchData() {
   try {
--- src/components/sign-in/components/ForgotPassword.jsx
+++ src/components/sign-in/components/ForgotPassword.jsx
@@ -1,51 +1,50 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { sendResetRequest } from '../../../api/user';
-import TextField from '@mui/material/TextField';
-import { useRef} from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { sendResetRequest } from "../../../api/supabase/supabase-utils/user";
+import TextField from "@mui/material/TextField";
+import { useRef } from "react";
 
 function ForgotPassword({ open, handleClose, snackBarVisibility }) {
   const emailPrompt = useRef("");
   const [emailError, setEmailError] = React.useState(false);
-  const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
-  
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState("");
+
   function handleSendResetRequest() {
     const email = emailPrompt.current.value;
 
     let isValid = true;
     if (!email || !/\S+@\S+\.\S+/.test(email)) {
-    setEmailError(true);
-    setEmailErrorMessage('Please enter a valid email address.');
-    isValid = false;
-  } else {
-    setEmailError(false);
-    setEmailErrorMessage('');
-  }
-  if(isValid) {
-    try {
-      sendResetRequest(email);
-      snackBarVisibility(() => {
-        return {
-          display: true,
+      setEmailError(true);
+      setEmailErrorMessage("Please enter a valid email address.");
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage("");
+    }
+    if (isValid) {
+      try {
+        sendResetRequest(email);
+        snackBarVisibility(() => {
+          return {
+            display: true,
             content: `Password reset link has been sent to ${email}`,
-            severity: "info"
-          }
+            severity: "info",
+          };
         });
         handleClose();
-      }
-      catch(e) {
+      } catch (e) {
         snackBarVisibility(() => {
           return {
             display: true,
             content: `Error while sending password reset link`,
-            severity: "error"
-          }
+            severity: "error",
+          };
         });
       }
     }
@@ -56,36 +55,36 @@ function ForgotPassword({ open, handleClose, snackBarVisibility }) {
       onClose={handleClose}
       slotProps={{
         paper: {
-          component: 'form',
+          component: "form",
           onSubmit: (event) => {
             event.preventDefault();
             handleClose();
           },
-          sx: { backgroundImage: 'none' },
+          sx: { backgroundImage: "none" },
         },
       }}
     >
       <DialogTitle>Reset password</DialogTitle>
       <DialogContent
-        sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
+        sx={{ display: "flex", flexDirection: "column", gap: 2, width: "100%" }}
       >
         <DialogContentText>
-          Enter your account&apos;s email address, and we&apos;ll send you a link to
-          reset your password.
+          Enter your account&apos;s email address, and we&apos;ll send you a
+          link to reset your password.
         </DialogContentText>
         <TextField
-            required
-            fullWidth
-            id="email"
-            placeholder="your@email.com"
-            name="email"
-            autoComplete="email"
-            variant="outlined"
-            error={emailError}
-            helperText={emailErrorMessage}
-            inputRef={emailPrompt}
-            color={emailError ? 'error' : 'primary'}
-          />
+          required
+          fullWidth
+          id="email"
+          placeholder="your@email.com"
+          name="email"
+          autoComplete="email"
+          variant="outlined"
+          error={emailError}
+          helperText={emailErrorMessage}
+          inputRef={emailPrompt}
+          color={emailError ? "error" : "primary"}
+        />
       </DialogContent>
       <DialogActions sx={{ pb: 3, px: 3 }}>
         <Button onClick={handleClose}>Cancel</Button>
