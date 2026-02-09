67a8612aacc1831b1328640a8812abca206ff49f
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2026-02-09 09:02:46 +0300

    Fixed tailwindcss checking range

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/TodoListComponent.jsx",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
